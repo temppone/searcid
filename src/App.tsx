5106cb258f3bac373b2fa14919d3101825c19266
temppone <gtvtempone@gmail.com>
2021-12-18 20:16:15 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      teste
-    </div>
-  );
-}
+const Home = lazy(() => import('./pages/Home'));
+
+const App = function () {
+    return (
+        <div className='App'>
+            <Router>
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                    </Routes>
+                </Suspense>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = function () {
+    return <div>essa é a home</div>;
+};
+
+export default Home;
